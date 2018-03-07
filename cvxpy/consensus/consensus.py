Fix a sign flip bug

cvxpy/consensus/consensus.py
@@ -57,7 +57,7 @@ def step_ls(p, d):
 	if 2*mg > sd:
 		return mg
 	else:
-		return (sd - mg)
+		return (sd - mg/2)
 
 def step_cor(p, d):
 	"""Correlation coefficient.
@@ -74,7 +74,7 @@ def step_cor(p, d):
 	float
 	     The correlation between two vectors.
 	"""
-	return np.sum(p*d)/np.sqrt(np.sum(p**2)*np.sum(d**2))
+	return np.sum(p*d)/(np.linalg.norm(p)*np.linalg.norm(d))
 
 def step_safe(rho, a, b, a_cor, b_cor, eps = 0.2):
 	"""Safeguarding rule for spectral step size update.
@@ -253,15 +253,15 @@ def run_worker(pipe, p, rho_init, *args, **kwargs):
 		pipe.send((prox.status, xvals))
 		xbars, i = pipe.recv()
 		
-		# Update u^(k+1) += rho^(k)*(x^(k+1) - x_bar^(k+1)).
+		# Update u^(k+1) += rho^(k)*(x_bar^(k+1) - x^(k+1)).
 		v_flat = {"x": [], "xbar": [], "u": [], "uhat": []}
 		ssq = {"primal": 0, "dual": 0, "x": 0, "xbar": 0, "u": 0}
 		for key in v.keys():
 			# Calculate primal/dual residual^(k+1).
 			if v[key]["x"].value is None:
-				primal = -xbars[key]
+				primal = xbars[key]
 			else:
-				primal = (v[key]["x"] - xbars[key]).value
+				primal = (xbars[key] - v[key]["x"]).value
 			dual = (rho*(v[key]["xbar"] - xbars[key])).value
 			
 			# Set parameter values of x_bar^(k+1) and u^(k+1).
@@ -269,7 +269,7 @@ def run_worker(pipe, p, rho_init, *args, **kwargs):
 			u_old = v[key]["u"].value
 			
 			v[key]["xbar"].value = xbars[key]
-			v[key]["u"].value += (rho*(v[key]["x"] - v[key]["xbar"])).value
+			v[key]["u"].value += (rho*(v[key]["xbar"] - v[key]["x"])).value
 			
 			# Save stopping rule criteria.
 			ssq["primal"] += np.sum(np.square(primal))
@@ -280,7 +280,7 @@ def run_worker(pipe, p, rho_init, *args, **kwargs):
 			ssq["u"] += np.sum(np.square(v[key]["u"].value))
 			
 			# Intermediate variable for step size update.
-			u_hat = u_old + rho*(v[key]["x"] - xbar_old)
+			u_hat = u_old + rho*(xbar_old - v[key]["x"])
 			v_flat["uhat"] += [np.asarray(u_hat.value).reshape(-1)]
 		pipe.send(ssq)
 		
@@ -344,4 +344,4 @@ def consensus(p_list, *args, **kwargs):
 	end = time()
 
 	[p.terminate() for p in procs]
-	return {"xbars": xbars, "residuals": resid[:(i+1),:], "solve_time": (end - start)}
+	return {"xbars": xbars, "residuals": resid[:(i+1),:], "iterations": i+1, "solve_time": (end - start)}

cvxpy/consensus/test_consensus.py
@@ -30,13 +30,14 @@ def compare_results(probs, obj_admm, obj_comb, x_admm, x_comb):
 		print "MSE: ", np.mean(np.square(x_admm[i] - x_comb[i])), "\n"
 	print "ADMM Objective: %f" % obj_admm
 	print "Base Objective: %f" % obj_comb
-	print "Elapsed Time: %f" % probs.solver_stats
+	print "Iterations: %d" % probs.solver_stats["iterations"]
+	print "Elapsed Time: %f" % probs.solver_stats["solve_time"]
 
 def basic_test():
 	np.random.seed(1)
 	m = 100
 	n = 10
-	MAX_ITER = 10
+	MAX_ITER = 100
 	x = Variable(n)
 	y = Variable(n/2)
 
@@ -68,9 +69,9 @@ def basic_test():
 def ols_test():
 	np.random.seed(1)
 	N = 2
-	m = N*100
+	m = N*1000
 	n = 10
-	MAX_ITER = 20
+	MAX_ITER = 100
 	x = Variable(n)
 	
 	# Problem data.
@@ -83,14 +84,14 @@ def ols_test():
 	# List of all the problems with objective f_i.
 	p_list = []
 	for A_sub, b_sub in zip(A_split, b_split):
-		p_list += [Problem(Minimize(sum_squares(A_sub*x-b_sub)))]
+		p_list += [Problem(Minimize(0.5*sum_squares(A_sub*x-b_sub) + 1.0/N*norm(x,1)))]
 		
 	probs = Problems(p_list)
 	probs.pretty_vars()
 	
 	# Solve with consensus ADMM.
-	obj_admm = probs.solve(method = "consensus", rho_init = N*[1.0], \
-						   max_iter = MAX_ITER, spectral = True)
+	obj_admm = probs.solve(method = "consensus", rho_init = N*[0.5], \
+						   max_iter = MAX_ITER, spectral = False)
 	x_admm = [x.value for x in probs.variables()]
 	
 	# Solve combined problem.
@@ -103,7 +104,7 @@ def ols_test():
 
 def lasso_test():
 	np.random.seed(1)
-	m = 100
+	m = 1000
 	n = 10
 	MAX_ITER = 10
 	DENSITY = 0.75
@@ -135,11 +136,11 @@ def lasso_test():
 	# Compare results.
 	compare_results(probs, obj_admm, obj_comb, x_admm, x_comb)
 
-print "Basic Test"
-basic_test()
+# print "Basic Test"
+# basic_test()
 
-# print "OLS Test"
-# ols_test()
+print "OLS Test"
+ols_test()
 
-print "Lasso Test"
-lasso_test()
+# print "Lasso Test"
+# lasso_test()