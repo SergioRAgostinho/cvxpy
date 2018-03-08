Fix a bug in calculation of x_bar. Add residual plotting function to Problems class.

cvxpy/consensus/problems.py
@@ -18,6 +18,7 @@ along with CVXPY.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 import cvxpy.utilities as u
 from cvxpy.problems.problem import Problem, Minimize
 from collections import defaultdict
@@ -233,5 +234,19 @@ class Problems(object):
 		self._dual_residual = solution["residuals"][:,1]
 		
 		# TODO: Handle statuses.
-		self._solver_stats = {"iterations": solution["iterations"],
+		self._solver_stats = {"num_iters": solution["num_iters"],
 							  "solve_time": solution["solve_time"]}
+	
+	def plot_residuals(self):
+		"""Plot the sum of squared primal/dual residuals over all iterations.
+		"""
+		if self._solver_stats is None:
+			raise ValueError("Solver stats is empty. Nothing to plot.")
+		iters = range(self._solver_stats["num_iters"])
+		resid = np.column_stack((self._primal_residual, self._dual_residual))
+		
+		plt_resd = plt.plot(iters, resid, label = ["Primal", "Dual"])
+		plt.legend(plt_resd, ["Primal", "Dual"])
+		plt.xlabel("Iteration")
+		plt.ylabel("Residual")
+		plt.show()

cvxpy/consensus/test_consensus.py
@@ -30,7 +30,7 @@ def compare_results(probs, obj_admm, obj_comb, x_admm, x_comb):
 		print "MSE: ", np.mean(np.square(x_admm[i] - x_comb[i])), "\n"
 	print "ADMM Objective: %f" % obj_admm
 	print "Base Objective: %f" % obj_comb
-	print "Iterations: %d" % probs.solver_stats["iterations"]
+	print "Iterations: %d" % probs.solver_stats["num_iters"]
 	print "Elapsed Time: %f" % probs.solver_stats["solve_time"]
 
 def basic_test():
@@ -71,7 +71,7 @@ def ols_test():
 	N = 2
 	m = N*1000
 	n = 10
-	MAX_ITER = 100
+	MAX_ITER = 10
 	x = Variable(n)
 	
 	# Problem data.
@@ -90,9 +90,10 @@ def ols_test():
 	probs.pretty_vars()
 	
 	# Solve with consensus ADMM.
-	obj_admm = probs.solve(method = "consensus", rho_init = N*[0.5], \
+	obj_admm = probs.solve(method = "consensus", rho_init = N*[0.05], \
 						   max_iter = MAX_ITER, spectral = False)
 	x_admm = [x.value for x in probs.variables()]
+	probs.plot_residuals()
 	
 	# Solve combined problem.
 	# obj_comb = Problem(Minimize(sum_squares(A*x-b))).solve()
@@ -104,9 +105,9 @@ def ols_test():
 
 def lasso_test():
 	np.random.seed(1)
-	m = 1000
+	m = 100
 	n = 10
-	MAX_ITER = 10
+	MAX_ITER = 100
 	DENSITY = 0.75
 	x = Variable(n)
 	
@@ -125,9 +126,10 @@ def lasso_test():
 	N = len(p_list)
 	
 	# Solve with consensus ADMM.
-	obj_admm = probs.solve(method = "consensus", rho_init = N*[1.0], \
-						   max_iter = MAX_ITER, spectral = True)
+	obj_admm = probs.solve(method = "consensus", rho_init = N*[0.5], \
+						   max_iter = MAX_ITER, spectral = False)
 	x_admm = [x.value for x in probs.variables()]
+	# probs.plot_residuals()
 	
 	# Solve combined problem.
 	obj_comb = probs.solve(method = "combined")